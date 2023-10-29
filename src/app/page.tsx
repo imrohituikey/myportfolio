41f629f8e2b1e29bec824c7a4b3972f9aacdb3f7
imrohituikey <rohituaike81@gmail.com>
2023-10-29 15:39:04 +0530

    routes created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import Image from 'next/image'
 import Nav from './components/Nav';
 import Homepage from './pages/Homepage'
 import Contact from './pages/Contact'
+import Projects from './pages/Projects'
+import Skills from './pages/Skills'
 
 export default function Home() {
   return (
    <main className='page bg-site bg-cover bg-no-repeat'>
     <Nav/>
     <Homepage/>
+    <Skills/>
+    <Projects/>
     <Contact/>
    </main>
   )
