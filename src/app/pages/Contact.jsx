00e1b55e36da3bd8acd65a6f30706a91be839a63
imrohituikey <rohituaike81@gmail.com>
2023-11-05 23:31:31 +0530

    main merge

---
+++ src/app/pages/Contact.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { contactdata } from "../../../Constants";
+import Image from "next/image";
+import Link from "next/link";
+import { Dots, Rings } from "../components";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../variants";
+const Contact = () => {
+  return (
+    <div
+      id="contact"
+      className="page xl:py-12 xl:p-12 text-center bg-[#02050A] text-white"
+    >
+      <div className="flex flex-col gap-8 xl:gap-12 items-center w-full h-full">
+        <div className="flex flex-col xl:flex-row gap-3 w-full justify-center px-6">
+          {contactdata.map((contact,index) => (
+            <motion.div
+              variants={fadeIn("down", 0.05)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              key={index}
+              className="link transition delay-100 ease-in-out flex flex-col items-center justify-center text-sm bg-[#09101A] rounded-md xl:h-[12rem] xl:w-[24rem] gap-2 text-center"
+            >
+              <Image
+                src={`/images/${contact.icon}`}
+                width={20}
+                height={20}
+                alt="image"
+                
+              />
+              <h3>{contact.name}</h3>
+              <p className="hover:text-black">{contact.data}</p>
+            </motion.div>
+          ))}
+        </div>
+        <div className="absolute left-0 ">
+          <Rings/>
+        </div>
+        <div className=" flex flex-col gap-4 xl:gap-6 w-full text-center items-center">
+          <h1 className="text-2xl xl:text-4xl font-semibold">
+            <span className="text-accent">let&apos;s </span>Connect
+          </h1>
+          <form className=" flex flex-col gap-6 xl:w-[60%] p-3 xl:p-8 rounded-md">
+            <div className="flex gap-4 xl:gap-6">
+              <input type="text" placeholder="Your Name" className="input" />
+              <input type="email" placeholder="Your Email" className="input" />
+            </div>
+            <input type="text" placeholder="Subject" className="input" />
+            <textarea
+              type="message"
+              placeholder="Your message"
+              className="textarea"
+            />
+          </form>
+          <div className="absolute right-5">
+            <Dots />
+          </div>
+          <button className="link px-3 py-1 xl:px-8 xl:py-2 bg-purple-400 text-black rounded-md text-sm">
+            Submit
+          </button>
+          <div className="absolute left-2">
+            <Dots />
+          </div>
+        </div>
+      </div>
+      <hr className="border-xl border-teal-500" />
+      <div className="flex justify-between pt-3 w-full">
+        <h1>Developed with love by Rohit © 2023</h1>
+        <div className="flex gap-12">
+          <Link href={"/"}>Terms & Condition</Link>
+          <Link href={"/"}>Privacy Policy</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/app/pages/Services.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div
+      id="services"
+      className="px-6 md:px-[12rem] bg-[#02050A] text-white  flex flex-col justify-center items-center gap-4"> 
+      <h1 className="self-center text-2xl py-3">My Services</h1>
+      <div className="flex flex-col gap-4">
+        <div className="items-center text-center">
+          <h1 className="md:text-5xl uppercase link transition ease-in-out rounded-full">
+            What I do
+          </h1>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-between w-full gap-6">
+        <div className="flex flex-col md:flex-row  justify-between gap-6">
+          <div className="bg-[#09101A] flex flex-col p-12 justify-around border rounded-md gap-4">
+            <div className="pb-2 w-[10rem] border-b-2 border-teal">
+              <h1>Creative Agency 01</h1>
+            </div>
+            <div>Framer Designer & Developer</div>
+            <div>2019 - Present</div>
+            <div className="w-[auto]">
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit sed thisnquia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt
+            </div>
+          </div>
+
+          <div className="bg-[#09101A] flex flex-col p-12 justify-around border rounded-md gap-4">
+            <div className="pb-2 w-[10rem] border-b-2 border-teal">
+              <h1>Creative Agency 01</h1>
+            </div>
+
+            <div>Framer Designer & Developer</div>
+            <div>2019 - Present</div>
+            <div className="w-[auto]">
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit sed thisnquia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row md:flex justify-between gap-6">
+          <div className=" bg-[#09101A] flex flex-col p-12 justify-around border rounded-md gap-4">
+            <div className="pb-2 w-[10rem] border-b-2 border-teal">
+              <h1>Creative Agency</h1>
+            </div>
+            <div>Framer Designer & Developer</div>
+            <div>2019 - Present</div>
+            <div className="w-[auto]">
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit sed thisnquia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt
+            </div>
+          </div>
+
+          <div className="bg-[#09101A] flex flex-col p-12 justify-around border rounded-md gap-4">
+            <div className="pb-2 w-[10rem] border-b-2 border-teal">
+              <h1>Creative Agency</h1>
+            </div>
+
+            <div>Framer Designer & Developer</div>
+            <div>2019 - Present</div>
+            <div className="w-[auto]">
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit sed thisnquia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
